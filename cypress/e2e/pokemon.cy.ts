307311e312eddc7a1eb5507e969317bdf62cddb1
Robbie Jaeger <jaeger.rob@gmail.com>
2023-08-05 20:25:01 -0600

    Add cypress tests for single and multiple page requests

--- cypress/e2e/pokemon.cy.ts
+++ cypress/e2e/pokemon.cy.ts
@@ -1,6 +1,30 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000');
+describe("Happy Path Results", () => {
+  it("Displays a single request result", () => {
+    cy.visit("http://localhost:3000");
     cy.contains("Pokemon Pursuit");
-  })
-})
+  
+    // Intercept request with no next page
+    cy.intercept("https://hungry-woolly-leech.glitch.me/api/pokemon/search/a", { fixture: "a.json" });
+  
+    cy.get("input").type("a");
+    cy.contains("Loading results...");
+    cy.contains("Arbok");
+    cy.contains("Arcanine");
+  });
+
+  it("Displays a request result requiring next page", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Pokemon Pursuit");
+  
+    // Intercept request with next page
+    cy.intercept("https://hungry-woolly-leech.glitch.me/api/pokemon/search/b", { fixture: "b.json" });
+    cy.intercept("https://hungry-woolly-leech.glitch.me/api/pokemon/search/b?page=MTE=", { fixture: "b-next.json" });
+    
+    cy.get("input").type("b");
+    
+    // First page
+    cy.contains("Bulbasaur");
+    // Next page
+    cy.contains("Butterfree");
+  });
+});
